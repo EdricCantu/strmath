bff674edb72eec2a7d04332d0e8c92c105333510
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2025-03-30 02:32:35 -0500

    Update c.js

--- c.js
+++ c.js
@@ -168,7 +168,6 @@ class StrMath{
         const elem = base[elemInd];/*SIGILL when debugging and stepping here? Why!!!!!!! happens every time!
         debugger; new StrMath.StrBase(..."0123456789");
         */
-        debugger;
         includesNonStrings = includesNonStrings || (typeof(elem)!=="string") || (elem.length !== 1);
         // if already true, no need to evaluate further   |||||||||||                      ||||||
         //            if false, check if elem is not a string. if false and it is a string ||||||
